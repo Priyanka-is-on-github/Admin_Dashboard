0a59295295ed44e468df6cc63417f52c98e24580
Priyanka Singh <priyankasingh8885@gmail.com>
2023-12-18 16:18:12 +0530

    Add functionality to delete multiple row at one time

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, createContext } from "react";
 import EditData from "./EditData";
+import "./App.css";
+
+const DetailsContext = createContext();
 
 function App() {
-  const [details, setDetails] = useState([]); 
-  const [name, setName] = useState("Arone Miles");
+  const [details, setDetails] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -14,108 +16,78 @@ function App() {
       let datas = await data.json();
 
       setDetails(datas);
-     
     })();
   }, []);
 
-  function Delete(indexnum){
-   
-     
-   let newdata = details.filter((currele , index)=>{
-       
-      return(
-        index != indexnum   
-      )
+  let selarr = [];
 
-    })
-   
-    setDetails(newdata);
-   
-     
+  const SelData = (idnum, event) => {
+    let row = document.getElementById(idnum);
+
+    if (event.target.checked == true) {
+      row.style.backgroundColor = "lightgray";
+      selarr.push(idnum);
+    } else {
+      row.style.backgroundColor = "transparent";
+      selarr = selarr.filter((currele) => {
+        return currele != idnum;
+      });
+    }
+  };
+
+  function MulDel() {
+    let newarr = details.filter((currele) => {
+      return !selarr.includes(currele.id);
+    });
+
+    setDetails(newarr);
   }
-
-  
-  
-    const Edit =(indexnum)=>{
-    
-        let row = document.getElementById(indexnum); 
-       if( row.querySelector('#name').hasAttribute("readOnly")) 
-        {
-          row.querySelector('#name').removeAttribute("readOnly"); 
-          row.querySelector('#email').removeAttribute("readOnly"); 
-          row.querySelector('#role').removeAttribute("readOnly"); 
-           
-        }
-        else{
-          row.querySelector('#name').setAttribute("readOnly" , true); 
-          row.querySelector('#email').setAttribute("readOnly", true); 
-          row.querySelector('#role').setAttribute("readOnly" , true); 
-         
-        }
-          
-          console.log(row.querySelector('#name')); 
-    }   
-
-  
-
-  
 
   return (
     <>
+      <DetailsContext.Provider value={{ details, setDetails }}>
+        <div>
+          <input type="text" placeholder="Search..." />
+          <button
+            onClick={() => {
+              MulDel();
+            }}
+          >
+            delete
+          </button>
+        </div>
+        <table border={1} cellPadding={10} cellSpacing={10}>
+          <thead>
+            <tr>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
 
-      <table border={2}>
-        <thead>
-          <tr>
-            <td>
-              <input type="checkbox" />
-            </td>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {details.map((currele, index) => {
-            return (
-              // <tr   id = {index} key = {index} >  
-              //   <td>
-              //     <input type="checkbox" />
-              //   </td>
-              //   {details.length == 0 ? ( 
-              //     <>
-              //       <td>Name</td>
-              //       <td>Email</td> 
-              //       <td>Role</td> 
-              //     </>  
-              //   ) : (
-              //     <>
-                    
-              //       <td><input type="text"  id = 'name' onChange={(event)=>{  
-              //         console.log(event.target.value) 
-                         
-              //       }} readOnly value ={currele.name} /></td>  
-              //       <td><input type="email" id ='email' readOnly value ={currele.email} /></td> 
-                
-                    
-              //       <td><input type="text"  id='role'readOnly value = {currele.role}/></td> 
-              //     </>
-              //   )}
-
-              //   <td>
-              //     <button onClick={()=>{Edit(index)}}>edit</button> 
-              //     <button onClick={()=>{Delete(index)}}>delete</button>
-              //   </td>
-              // </tr>
-
-              <EditData name = {currele.name} email = {currele.email} role = {currele.role} id = {index} key = {index} editfun ={Edit} /> 
-            );
-          })}
-        </tbody>
-      </table>
+          <tbody>
+            {details.map((currele, index) => {
+              return (
+                <EditData
+                  name={currele.name}
+                  email={currele.email}
+                  role={currele.role}
+                  id={currele.id}
+                  key={currele.id}
+                  seldata={SelData}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </DetailsContext.Provider>
     </>
   );
 }
 
 export default App;
+export { DetailsContext };
--- src/EditData.jsx
+++ src/EditData.jsx
@@ -1,42 +1,108 @@
-import React ,{useState} from "react";
+import React, { useState, useContext, createContext } from "react";
 import App from "./App";
+import { DetailsContext } from "./App";
 
-const EditData = (props)=>{
-   
-  const [name , setName] = useState(props.name)
-  const [email , setEmail] = useState(props.email)
-  const [role , setRole] = useState(props.role)   
-          
-       return(
+const EditData = (props) => {
+  const userdetail = useContext(DetailsContext);
+  const { details, setDetails } = userdetail;
 
-        <>
-        
-            
-           
-             <tr id = {props.id}>
-             <td>
-                  <input type="checkbox" />
-                </td>
-             <td><input type="text"  id = 'name' onChange={(event)=>{
-                   setName(event.target.value)
-              }} readOnly value ={name} /></td>  
-              <td><input type="email" id ='email' readOnly value ={email} onChange={(event)=>{
-                  setEmail(event.target.value)
-              }} /></td> 
-          
-              
-              <td><input type="text"  id='role'readOnly value = {role} onChange = {(event)=>{
-                setRole(event.target.value)
-              }}/></td> 
-            
-            <td>
-                  <button onClick={()=>{props.editfun(props.id)}}>edit</button>  
-                  <button onClick={()=>{Delete(index)}}>delete</button> 
-                </td>
-             </tr>
-            
-           
-        </>
-    )
-}
+  const [editsave, setEditsave] = useState("✏️");
+
+  const [name, setName] = useState(props.name);
+  const [email, setEmail] = useState(props.email);
+  const [role, setRole] = useState(props.role);
+
+  const Delete = (indexnum) => {
+    let newdata = details.filter((currele) => {
+      return currele.id != indexnum;
+    });
+
+    setDetails(newdata);
+  };
+
+  const Edit = (indexnum) => {
+    let row = document.getElementById(indexnum);
+
+    if (row.querySelector("#name").hasAttribute("readOnly")) {
+      row.querySelector("#name").removeAttribute("readOnly");
+      row.querySelector("#email").removeAttribute("readOnly");
+      row.querySelector("#role").removeAttribute("readOnly");
+      setEditsave("📄");
+    } else {
+      row.querySelector("#name").setAttribute("readOnly", true);
+      row.querySelector("#email").setAttribute("readOnly", true);
+      row.querySelector("#role").setAttribute("readOnly", true);
+      setEditsave("✏️");
+    }
+  };
+
+  return (
+    <>
+      <tr id={props.id}>
+        <td id="inpt">
+          <input
+            type="checkbox"
+            style={{ backgroundColor: "transparent" }}
+            id="chkbox"
+            onClick={(event) => {
+              props.seldata(props.id, event);
+            }}
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            id="name"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+            readOnly
+            value={name}
+          />
+        </td>
+        <td>
+          <input
+            type="email"
+            id="email"
+            readOnly
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+        </td>
+
+        <td>
+          <input
+            type="text"
+            id="role"
+            readOnly
+            value={role}
+            onChange={(event) => {
+              setRole(event.target.value);
+            }}
+          />
+        </td>
+
+        <td>
+          <button
+            id="edit-save"
+            onClick={() => {
+              Edit(props.id);
+            }}
+          >
+            {editsave}
+          </button>
+          <button
+            onClick={() => {
+              Delete(props.id);
+            }}
+          >
+            🗑️
+          </button>
+        </td>
+      </tr>
+    </>
+  );
+};
 export default EditData;
